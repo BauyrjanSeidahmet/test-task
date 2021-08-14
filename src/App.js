52b41b58584875d9b53987175d1ee01569d21825
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 02:07:44 +0600

    layout was added to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Switch, Route} from "react-router-dom";
+import Layout from './components/Layout/Layout';
 import Categories from './containers/Categories/Categories';
 import Login from './containers/Login/Login';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path='/' component={Categories}/>
-      <Route exact path='/login' component={Login}/>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Categories}/>
+        <Route exact path='/login' component={Login}/>
+      </Switch>
+    </Layout>
   );
 }
 
