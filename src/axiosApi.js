c130d68995418082ebedb7cc2439dab40e678da5
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 00:04:39 +0600

    axios file was created

---
+++ src/axiosApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import store from "./store/configureStore";
+
+const instance = axios.create(
+    {baseURL: 'https://api.doover.tech'}
+)
+
+instance.interceptors.request.use(req => {
+    const users = store.getState().users;
+    if (users.user) req.headers["Authorization"] = users.user.token;
+    return req;
+  });
+
+export default instance;
