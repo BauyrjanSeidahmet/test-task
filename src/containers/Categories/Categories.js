209b56ab18d7412066b04b6cf1dee7b6ffca7dcc
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 02:18:32 +0600

    added useEffect for fetching categories

--- src/containers/Categories/Categories.js
+++ src/containers/Categories/Categories.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
 import { Grid, Typography } from '@material-ui/core';
+import { fetchCategories } from "../../store/actions/productsUser";
+
 
 const Categories = () => {
+  const dispatch = useDispatch();
+
+  const categories = useSelector((state) => state.products.categories);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+}, [dispatch]);
+
+console.log('categories', categories);
+
   return (
     <Grid>
       <Typography variant='h5'>
