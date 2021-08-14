9e0f17090c046cc7d950a8eeb92d5a92ffaa8c49
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 01:59:57 +0600

    layout was created

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Header from './Header';
+
+const Layout = ({ children }) => (
+  <div style={{maxWidth: '1000px', margin: '20px auto'}}>
+    <Header/>
+    <Container maxWidth='xl'>{children}</Container>
+  </div>
+);
+export default Layout;
