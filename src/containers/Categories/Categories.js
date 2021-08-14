dd8d1cdd59f1fdd118e6b66a9526b486190991c1
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 00:07:35 +0600

    categories page was created

---
+++ src/containers/Categories/Categories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+
+const Categories = () => {
+  return (
+    <Grid>
+      <Typography variant='h5'>
+        Categories page
+      </Typography>
+    </Grid>
+  );
+};
+
+export default Categories;
