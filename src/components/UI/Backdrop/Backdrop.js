dda4d5316c2319739f33a5b4580c489cc7e2d68b
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 22:18:02 +0600

    backdrop was created

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Backdrop.css'
+
+const Backdrop = ({show, clicked}) => {
+    return show ? <div className='Backdrop' onClick={clicked}/> : null
+    
+}
+
+export default Backdrop
