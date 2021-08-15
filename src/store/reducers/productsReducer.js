9927f7b5624353ac017b66e23dd980387bb0320b
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 17:44:33 +0600

    fetch products were added to reducer

--- src/store/reducers/productsReducer.js
+++ src/store/reducers/productsReducer.js
@@ -1,8 +1,10 @@
-import { FETCH_CATEGORIES_FAILURE, FETCH_CATEGORIES_SUCCESS } from "../actionTypes";
+import { FETCH_CATEGORIES_FAILURE, FETCH_CATEGORIES_SUCCESS, FETCH_PRODUCTS_FAILURE, FETCH_PRODUCTS_SUCCESS } from "../actionTypes";
 
 const initialState = {
   categories: [],
+  products: [],
   catError: null,
+  getProductError: null,
 };
 
 const reducer = (state = initialState, action) => {
@@ -11,6 +13,10 @@ const reducer = (state = initialState, action) => {
       return {...state, categories: action.categories};
     case FETCH_CATEGORIES_FAILURE:
       return {...state, catError: action.error};
+    case FETCH_PRODUCTS_SUCCESS:
+      return {...state, products: action.products};
+    case FETCH_PRODUCTS_FAILURE:
+      return {...state, getProductError: action.error};
     default:
       return state;
   }
