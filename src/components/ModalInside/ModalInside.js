2a53e441d7bfeae4e53270dd98cc523ce14be54a
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 22:19:01 +0600

    modalInside component was created

---
+++ src/components/ModalInside/ModalInside.js
@@ -0,0 +1,13 @@
+import './ModalInside.css';
+
+const ModalInside = ({ name, brand, description }) => {
+  return (
+    <div>
+      <h4>{name}</h4>
+      <p>Brand: {brand}</p>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default ModalInside;
