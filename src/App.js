94570a02a0208483cfe2bef1a4c7486a3f4d66a1
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 19:11:11 +0600

    added route for products

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Switch, Route} from "react-router-dom";
 import Layout from './components/Layout/Layout';
+import ProductsItem from './components/ProductItem/ProductItem';
 import Categories from './containers/Categories/Categories';
 import Login from './containers/Login/Login';
+import Products from './containers/Products/Products';
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Categories}/>
         <Route exact path='/login' component={Login}/>
+        <Route exact path='/products' component={Products}/>
       </Switch>
     </Layout>
   );
