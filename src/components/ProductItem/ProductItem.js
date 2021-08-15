8c6b3106a586525099a0274e85fdd5778bb63253
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-15 22:16:59 +0600

    modal was added

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './ProductItem.css';
-import PlusIcon from '../../assests/images/plus.svg'
-import MinusIcon from '../../assests/images/minus.svg'
+import PlusIcon from '../../assets/images/plus.svg'
+import MinusIcon from '../../assets/images/minus.svg'
+import { useState } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import ModalInside from '../ModalInside/ModalInside';
 
-const ProductItem = ({ image, name }) => {
+const ProductItem = ({ image, name, brand, description }) => {
+  const [showModal, setShowModal] = useState(false);
   const readyImage = `https://api.doover.tech${image}`;
+
+  const onClickProduct = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
   return (
     <div className='card'>
+      <Modal
+          show={showModal}
+          close={closeModal}
+          children={<ModalInside image ={readyImage} name={name} brand={brand} description={description}/>}
+      />
       <img src={readyImage} alt='Avatar' style={{ width: '100%' }} className='catImage'/>
+      <button onClick={onClickProduct}>?</button>
       <div className='container'>
         <h4>
           <b>{name}</b>
