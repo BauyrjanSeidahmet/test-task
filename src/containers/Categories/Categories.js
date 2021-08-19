1ac222d96b4603deaf1c2ba54522e35693c4bdf8
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:40:03 +0600

    добавил key атрибут

--- src/containers/Categories/Categories.js
+++ src/containers/Categories/Categories.js
@@ -2,26 +2,24 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CategoryItem from '../../components/CategoryItem/CategoryItem';
 import { fetchCategories } from '../../store/actions/productsActions';
-import './Categories.css'
+import './Categories.css';
 
 const Categories = () => {
   const dispatch = useDispatch();
 
-  const categories = useSelector(state => state.products.categories);
+  const categories = useSelector((state) => state.products.categories);
 
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
 
-  return <div className='Categories'>
-    {categories.map(cat => {
-      return <CategoryItem
-          id={cat.uuid}
-          image={cat.picture}
-          name={cat.name}
-      />
-    })}
-  </div>;
+  return (
+    <div className='Categories'>
+      {categories.map((cat) => {
+        return <CategoryItem key={cat.uuid} id={cat.uuid} image={cat.picture} name={cat.name} />;
+      })}
+    </div>
+  );
 };
 
 export default Categories;
