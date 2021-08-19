24ce624fd30b76ccf13524ec8794a3d6041dcdea
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:42:34 +0600

    временно поставил токен

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import store from "./store/configureStore";
+// import store from './store/configureStore';
 
-const instance = axios.create(
-    {baseURL: 'https://api.doover.tech'}
-)
+const instance = axios.create({ baseURL: 'https://api.doover.tech' });
 
-instance.interceptors.request.use(req => {
-    const users = store.getState().users;
-    if (users.user) req.headers["Authorization"] = `Bearer ${users.user.token}`;
-    return req;
-  });
+instance.interceptors.request.use((req) => {
+  // const users = store.getState().users;
+  // if (users.user) req.headers['Authorization'] = `Bearer ${users.user.token}`;
+  req.headers['Authorization'] =
+    'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjI5NDc3NjE3LCJqdGkiOiI0OTVjYzIwMzk1MjA0OGVhYjI3OWRmNDQ5YmU4M2ZhZSIsInVzZXJfaWQiOiJiZGY2ZjVjNS0wMmM1LTQ2ZTgtOGNlMy1kYmE4YjFlNjJhMGUifQ.YkUJWi0n2oRRwnYD3Gxy-dI09OXXkwAfUIkYB_9HOeI';
+  return req;
+});
 
 export default instance;
