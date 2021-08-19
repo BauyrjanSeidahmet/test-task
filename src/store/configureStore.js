11d53df9c273680dec3b2c6a90600baecc8bfe39
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:49:09 +0600

    eslint форматирование в configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,25 +1,22 @@
-import {createBrowserHistory} from "history";
-import {applyMiddleware, combineReducers, compose, createStore} from "redux";
-import {connectRouter, routerMiddleware} from "connected-react-router";
-import thunkMiddleware from "redux-thunk";
-import {loadFromLocalStorage, saveToLocalStorage} from "./localStorage";
-import usersReducers from "./reducers/usersReducer";
-import productsReducers from "./reducers/productsReducer";
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import thunkMiddleware from 'redux-thunk';
+import { loadFromLocalStorage, saveToLocalStorage } from './localStorage';
+import usersReducers from './reducers/usersReducer';
+import productsReducers from './reducers/productsReducer';
 
 export const history = createBrowserHistory();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-    users: usersReducers,
-    products: productsReducers,
-    router: connectRouter(history)
+  users: usersReducers,
+  products: productsReducers,
+  router: connectRouter(history),
 });
 
-const middleware = [
-    thunkMiddleware,
-    routerMiddleware(history)
-];
+const middleware = [thunkMiddleware, routerMiddleware(history)];
 
 const enhancers = composeEnhancers(applyMiddleware(...middleware));
 
@@ -28,11 +25,11 @@ const persistedState = loadFromLocalStorage();
 const store = createStore(rootReducer, persistedState, enhancers);
 
 store.subscribe(() => {
-    saveToLocalStorage({
-        users: {
-            user: store.getState().users.user
-        }
-    });
+  saveToLocalStorage({
+    users: {
+      user: store.getState().users.user,
+    },
+  });
 });
 
 export default store;
