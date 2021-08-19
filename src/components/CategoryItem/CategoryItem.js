b6bebd7781352aa95c7ac3bb465b0edc5e7cfa0f
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:46:06 +0600

    форматирование

--- src/components/CategoryItem/CategoryItem.js
+++ src/components/CategoryItem/CategoryItem.js
@@ -6,7 +6,7 @@ const CategoryItem = ({ image, name, id }) => {
   const readyImage = `https://api.doover.tech${image}`;
   return (
     <NavLink className='card' to={`/products?category=${id}`}>
-      <img src={readyImage} alt='Avatar' style={{ width: '100%' }} className='catImage'/>
+      <img src={readyImage} alt='Avatar' style={{ width: '100%' }} className='catImage' />
       <div className='container'>
         <h4>
           <b>{name}</b>
