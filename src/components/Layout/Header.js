9eefe2fa7bdf6cb3271450ee96e4761f4e24590c
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:20:16 +0600

    eslint форматирования

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Button, Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import SearchBar from '../SearchBar/SearchBar';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
   },
   logo: {
     flexGrow: 1,
-    color: 'inherit'
+    color: 'inherit',
   },
   btn: {
     marginRight: 10,
@@ -27,42 +27,40 @@ const Header = () => {
   const user = useSelector((state) => state.users.user);
 
   return (
-        <Grid container justifyContent='space-between' direction='row'>
-          <Grid container item xs={9}>
-            <Button
-              component={Link}
-              to='/'
-              color='inherit'
-              className={classes.btn}
-            >
-              Главная
+    <Grid container justifyContent='space-between' direction='row'>
+      <Grid container item xs={9}>
+        <Button component={Link} to='/' color='inherit' className={classes.btn}>
+          Главная
+        </Button>
+        <Button component={Link} to='/basket' color='inherit' className={classes.btn}>
+          Корзина
+        </Button>
+      </Grid>
+      <Grid item>
+        <Typography variant='h6' className={classes.logo}>
+          CONCEPT
+        </Typography>
+      </Grid>
+      <Grid item>
+        <SearchBar />
+      </Grid>
+      <Grid item>
+        {user ? (
+          <>
+            <Typography>Hello, user</Typography>
+            <Button color='inherit' className={classes.menuButton}>
+              Выйти
             </Button>
-            <Button component={Link} to='/basket' color='inherit' className={classes.btn}>
-              Корзина
+          </>
+        ) : (
+          <>
+            <Button to='/login' component={Link} color='inherit' className={classes.menuButton}>
+              Войти
             </Button>
-          </Grid>
-          <Grid item>
-            <Typography variant='h6' className={classes.logo}>
-              CONCEPT
-            </Typography>
-          </Grid>
-          <Grid item>
-            {user ? (
-              <>
-                <Typography>Hello, user</Typography>
-                <Button color='inherit' className={classes.menuButton}>
-                  Выйти
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button to='/login' component={Link} color='inherit' className={classes.menuButton}>
-                  Войти
-                </Button>
-              </>
-            )}
-          </Grid>
-        </Grid>
+          </>
+        )}
+      </Grid>
+    </Grid>
   );
 };
 
