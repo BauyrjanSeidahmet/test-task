8d1b687abe5d17e8629b031717b5e5372a4a395b
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:49:48 +0600

    eslint форматирование в usersReducer

--- src/store/reducers/usersReducer.js
+++ src/store/reducers/usersReducer.js
@@ -1,19 +1,19 @@
-import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE } from "../actionTypes";
+import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE } from '../actionTypes';
 
 const initialState = {
-    loginError: null,
-    user: null
+  loginError: null,
+  user: null,
 };
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case LOGIN_USER_SUCCESS:
-            return {...state, user: action.user, loginError: null};
-        case LOGIN_USER_FAILURE:
-            return {...state, loginError: action.error};
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.user, loginError: null };
+    case LOGIN_USER_FAILURE:
+      return { ...state, loginError: action.error };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
