2df64c8759ad31e0f4c06226c99b0a8ae5e0e0a6
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:44:11 +0600

    форматрование

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { useState } from 'react';
-import SearchIcon from '../../assets/images/search.svg'
-import './SearchBar.css'
+import SearchIcon from '../../assets/images/search.svg';
+import './SearchBar.css';
 import { NavLink } from 'react-router-dom';
 
 const SearchBar = () => {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
 
   const onChange = (e) => {
     const { value } = e.target;
     setInput(value);
-  }
+  };
 
   return (
-      <div>
-        <input value={input} onChange={onChange}/>
-        <NavLink to={`/products?search=${input}`}>
-          <img alt='search icon' src={SearchIcon}/>
-        </NavLink>
-      </div>
+    <div>
+      <input value={input} onChange={onChange} />
+      <NavLink to={`/products?search=${input}`}>
+        <img alt='search icon' src={SearchIcon} />
+      </NavLink>
+    </div>
   );
 };
 
