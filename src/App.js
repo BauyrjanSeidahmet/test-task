6607dae22042f0cd54fc8d8494d5cd202c74f9e1
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:45:20 +0600

    форматирование в  App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from "react-router-dom";
+import { Switch, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Categories from './containers/Categories/Categories';
 import Login from './containers/Login/Login';
@@ -9,12 +9,12 @@ const App = () => {
   return (
     <Layout>
       <Switch>
-        <Route exact path='/' component={Categories}/>
-        <Route exact path='/login' component={Login}/>
-        <Route exact path='/products' component={Products}/>
+        <Route exact path='/' component={Categories} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/products' component={Products} />
       </Switch>
     </Layout>
   );
-}
+};
 
 export default App;
