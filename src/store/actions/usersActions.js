60da09eb50fec7406cb14c62681ad7055a7440ac
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 00:11:22 +0600

    добавил content type на post запрос

--- src/store/actions/usersActions.js
+++ src/store/actions/usersActions.js
@@ -1,22 +1,31 @@
-import axios from "../../axiosApi";
-import {push} from "connected-react-router";
-import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE } from "../actionTypes";
+import axios from '../../axiosApi';
+import { push } from 'connected-react-router';
+import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE } from '../actionTypes';
 
-const loginUserSuccess = user => {
-    return {type: LOGIN_USER_SUCCESS, user};
+const loginUserSuccess = (user) => {
+  return { type: LOGIN_USER_SUCCESS, user };
 };
-const loginUserFailure = error => {
-    return {type: LOGIN_USER_FAILURE, error};
+const loginUserFailure = (error) => {
+  return { type: LOGIN_USER_FAILURE, error };
 };
 
-export const loginUser = userData => {
-    return async dispatch => {
-        try {
-            const response = await axios.post("/api/token", userData);
-            dispatch(loginUserSuccess(response.data))
-            dispatch(push("/"));
-        } catch(error) {
-            dispatch(loginUserFailure(error));
-        }
+export const loginUser = (userData) => {
+  return async (dispatch) => {
+    const headers = {
+      'content-Type': 'application/json',
     };
+    const responseType = 'text';
+    try {
+      const response = await axios.post('https://api.doover.tech/api/token', userData, {
+        responseType,
+        headers: {
+          ...headers,
+        },
+      });
+      dispatch(loginUserSuccess(response.data));
+      dispatch(push('/'));
+    } catch (error) {
+      dispatch(loginUserFailure(error));
+    }
+  };
 };
