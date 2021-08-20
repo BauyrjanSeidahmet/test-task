c63545347ae4f805c5e17271d47a894a019d1a0c
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-20 23:45:42 +0600

    protected routes were added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Cart from './containers/Cart/Cart';
 import Categories from './containers/Categories/Categories';
 import Login from './containers/Login/Login';
 import Products from './containers/Products/Products';
 
+const ProtectedRoute = ({ isAllowed, redirectTo, ...props }) => {
+  return isAllowed ? <Route {...props} /> : <Redirect to={redirectTo} />;
+};
+
 const App = () => {
+  const user = useSelector((state) => state.users.user);
+
   return (
     <Layout>
       <Switch>
-        <Route exact path='/' component={Categories} />
+        <ProtectedRoute isAllowed={user} redirectTo={'/login'} path='/' exact component={Categories} />
+        <ProtectedRoute isAllowed={user} redirectTo={'/login'} path='/products' exact component={Products} />
+        <ProtectedRoute isAllowed={user} redirectTo={'/login'} path='/cart' exact component={Cart} />
         <Route exact path='/login' component={Login} />
-        <Route exact path='/products' component={Products} />
-        <Route exact path='/cart' component={Cart} />
       </Switch>
     </Layout>
   );
