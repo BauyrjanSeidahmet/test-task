6fd87ad45a5765425598532b96abd794f06fc3d8
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-21 15:58:39 +0600

    actions for logout was added

--- src/store/actions/usersActions.js
+++ src/store/actions/usersActions.js
@@ -1,6 +1,6 @@
 import axios from '../../axiosApi';
 import { push } from 'connected-react-router';
-import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE } from '../actionTypes';
+import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE, LOGOUT_USER_SUCCESS, LOGOUT_USER_FAILURE } from '../actionTypes';
 
 const loginUserSuccess = (user) => {
   return { type: LOGIN_USER_SUCCESS, user };
@@ -26,6 +26,24 @@ export const loginUser = (userData) => {
       dispatch(push('/'));
     } catch (error) {
       dispatch(loginUserFailure(error));
+    }
+  };
+};
+
+const logoutUserSuccess = () => {
+  return { type: LOGOUT_USER_SUCCESS };
+};
+const logoutUserFailure = (error) => {
+  return { type: LOGOUT_USER_FAILURE, error };
+};
+
+export const logoutUser = () => {
+  return (dispatch) => {
+    try {
+      dispatch(logoutUserSuccess());
+      dispatch(push('/login'));
+    } catch (error) {
+      dispatch(logoutUserFailure(error));
     }
   };
 };
