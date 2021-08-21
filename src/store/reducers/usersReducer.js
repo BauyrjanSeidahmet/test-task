e4e5f89b3f418dc38a37ef98998ed7d62723e78e
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-21 15:55:15 +0600

    added actions to logout

--- src/store/reducers/usersReducer.js
+++ src/store/reducers/usersReducer.js
@@ -1,7 +1,8 @@
-import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE } from '../actionTypes';
+import { LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE, LOGOUT_USER_SUCCESS, LOGOUT_USER_FAILURE } from '../actionTypes';
 
 const initialState = {
   loginError: null,
+  logoutError: null,
   user: null,
 };
 
@@ -11,6 +12,10 @@ const reducer = (state = initialState, action) => {
       return { ...state, user: action.user, loginError: null };
     case LOGIN_USER_FAILURE:
       return { ...state, loginError: action.error };
+    case LOGOUT_USER_SUCCESS:
+      return { initialState };
+    case LOGOUT_USER_FAILURE:
+      return { ...state, logoutError: action.error };
     default:
       return state;
   }
